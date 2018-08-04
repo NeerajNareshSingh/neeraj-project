c796d76d98b1f6ee1d1381c9850dbf3cd4570abc
Neeraj <singh.neeraj433@gmail.com>
2018-08-04 15:39:28 +0530

    initial commit

---
+++ about.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About</title>
+</head>
+<body>
+	<h1>about us</h1>
+ <p>This is about page</p>
+ <p>wite here</p>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+ <p>This is home page,this</p>
+</body>
+</html>
